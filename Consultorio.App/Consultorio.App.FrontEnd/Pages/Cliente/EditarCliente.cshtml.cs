b3c17d4dce5decd4a591549f631912b58fbc329e
Diana <diana.lizeth.ocampo@gmail.com>
2021-09-21 19:09:44 -0500

    editar

---
+++ Consultorio.App/Consultorio.App.FrontEnd/Pages/Cliente/EditarCliente.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Consultorio.App.Persistencia;
+using Consultorio.App.Dominio;
+
+namespace MyApp.Namespace
+{
+    public class EditarClienteModel : PageModel
+    { private readonly IRepositorioCliente repositorioCliente;
+        public Cliente cliente{get; set;}
+        public EditarClienteModel(IRepositorioCliente repositorioCliente){
+            this.repositorioCliente = repositorioCliente;
+        }
+        public void OnGet(string  Documento)
+        {
+            cliente= repositorioCliente.GetCliente(Documento);
+        }
+        public IActionResult OnPost(Cliente cliente){
+            repositorioCliente.UpdateCliente(cliente);
+            return RedirectToPage("./Cliente");
+
+        }
+    }
+}
