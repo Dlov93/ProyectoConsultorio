ba296ad2be52c57a17b162f6295395d50e21d75c
Diana <diana.lizeth.ocampo@gmail.com>
2021-09-21 19:35:50 -0500

    agregar Cliente

---
+++ Consultorio.App/Consultorio.App.FrontEnd/Pages/Cliente/CrearCliente.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Consultorio.App.Dominio;
+using Consultorio.App.Persistencia;
+
+namespace MyApp.Namespace
+{
+    public class CrearClienteModel : PageModel
+    {
+       private readonly IRepositorioCliente repositorioCliente;
+        public Cliente cliente{get; set;}
+        public CrearClienteModel(IRepositorioCliente repositorioCliente){
+            this.repositorioCliente = repositorioCliente;
+        }
+        public void OnGet()
+        {
+            cliente= new Cliente();
+        }
+        public IActionResult OnPost(Cliente cliente){
+            repositorioCliente.AddCliente(cliente);
+            return RedirectToPage("./Cliente");
+        }
+    }
+}
